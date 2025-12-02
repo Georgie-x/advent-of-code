a365c04d8a293b8f879192b28603aa55c702bbf7
Georgie <georgiex@gmail.com>
2025-12-02 23:07:59 +0000

    end of day 2

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 Advent of Code 2025
 
+
+
+
+
+
+
+
+
+
+
+Notes:
+
 day2 pseudo
 
 convert input to array of arrays or array of strings?
 loop over
+
+
+
+// the second loop from the first part of day2 problem
+
+// for (let i = start; i <= end; i++) {
+// 			let splitIndex = Math.ceil(i.toString().length / 2)
+// 			let cHalf = i.toString().slice(0, splitIndex)
+// 			let dHalf = i.toString().slice(splitIndex)
+// 			let finalNum = Number(`${Number(cHalf)}${Number(dHalf)}`)
+// 			cHalf === dHalf ? (total += finalNum) : null
+// 		}
--- src/gift-shop.jsx
+++ src/gift-shop.jsx
@@ -2,22 +2,26 @@ import { d2input } from "../input"
 
 function DoubleNumbers() {
 	let total = 0
+	let nummos = []
+
 	for (let inputRange of d2input) {
 		let [aHalf, bHalf] = inputRange.split("-")
 		let start = Number(aHalf)
 		let end = Number(bHalf)
-		console.log(aHalf, bHalf)
 
-
-
-		for (let i = start; i >= end; i++) {
-			let splitIndex = Math.ceil(i.length / 2)
-			let cHalf = i.toString().slice(0, splitIndex)
-			let dHalf = i.toString().slice(splitIndex)
-			console.log(cHalf, dHalf)
-			cHalf === dHalf ? (total += 100) : null
+		for (let i = start; i <= end; i++) {
+			let iString = i.toString()
+			;(iString.length >= 2 && new Set(iString.match(/.{1,1}/g)).size === 1) ||
+			(iString.length >= 4 && new Set(iString.match(/.{1,2}/g)).size === 1) ||
+			(iString.length >= 6 && new Set(iString.match(/.{1,3}/g)).size === 1) ||
+			(iString.length >= 8 && new Set(iString.match(/.{1,4}/g)).size === 1) ||
+			(iString.length >= 10 && new Set(iString.match(/.{1,5}/g)).size === 1)
+				? ((total += i), nummos.push(i))
+				: null
 		}
 	}
+
+	console.log(nummos)
 
 	return <p>{total}</p>
 }
